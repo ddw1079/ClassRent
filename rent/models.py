bd45f3522f6d16d770a4db559c5ac74e0972a203
KHS <ddw1079@naver.com>
2019-11-22 18:01:59 +0900

    댓글 - 도중  https://tutorial-extensions.djangogirls.org/ko/homework_create_more_models/

--- rent/models.py
+++ rent/models.py
@@ -27,6 +27,20 @@ def publish(self):
     self.save()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey('rent.post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
 class Room(models.Model):
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
--- rent/views.py
+++ rent/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 
 # Create your views here.
@@ -52,6 +52,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'post_edit.html', {'form': form})
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'rent/add_comment_to_post.html', {'form': form})
 
 
 @login_required
